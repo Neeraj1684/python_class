8f49cd6f1ac6e1d4bf4295969ffd8597937554cf
Neeraj1684 <76860453+Neeraj1684@users.noreply.github.com>
2021-02-03 22:02:42 +0530

    Create assignment4.py

---
+++ assignment4.py
@@ -0,0 +1,13 @@
+list1 = ["hello", "take"]
+list2 = ["dear", "sir"]
+
+for item in list1:
+  for item1 in list2:
+    print(item+" "+item1)
+    
+ output
+ 
+#hello dear
+#hello sir
+#take dear
+#take sir
